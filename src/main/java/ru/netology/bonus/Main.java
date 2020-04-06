7efbae54ddf5156a47bd2c5b425e34eb73fb39fe
elenaslavutina <elena.slavutina2013@gmail.com>
2020-04-06 20:19:42 +0300

    init

---
+++ src/main/java/ru/netology/bonus/Main.java
@@ -0,0 +1,4 @@
+package ru.netology.bonus;
+
+public class Main {
+}
---
+++ src/test/java/ru/netology/bonus/BonusServiceTest.java
@@ -0,0 +1,26 @@
+package ru.netology.bonus;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class BonusServiceTest {
+    @ParameterizedTest
+    @CsvFileSource(resources = "/data.csv")
+
+    void shouldCalculate(String testname, long amount, boolean registered, long expected) {
+        BonusService service = new BonusService();
+
+        long actual = service.calculate(amount, registered);
+
+        assertEquals(expected, actual);
+    }
+
+   }
+
+
+
+
